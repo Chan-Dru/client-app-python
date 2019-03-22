8eac590be007a3b0862f3956735512d14846dc02
root <root@ip-172-31-33-5.us-west-2.compute.internal>
2019-03-22 08:00:55 +0000

    added logging file to client program

--- app.py
+++ app.py
@@ -2,18 +2,33 @@ import requests
 import time
 from multiprocessing import Process
 import os
+import logging
+import logging.handlers
+
+logger = logging.getLogger('client_program')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+handler = logging.handlers.TimedRotatingFileHandler("/home/centos/logs/client_log",'midnight',1)
+handler.suffix = "%Y-%m-%d"
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 processes = []
 thread = int(os.environ.get('THREAD',3))
-# domain_name = os.environ.get('DOMAIN_NAME','http://flaskapp.openshiftnode.gtrrt.click/')
-domain_name = os.environ.get('DOMAIN_NAME','http://flask-app-chaosproject.router.default.svc.cluster.local')
+domain_name = os.environ.get('DOMAIN_NAME','http://os-sample-python-python-flask.okdexperimentation.gtrrt.click')
 sleep_time = int(os.environ.get('SLEEP_TIME',3))
 
 def query_server(domainname):
     start = time.time()
     r = requests.get(domainname)
     roundtrip = time.time() - start
-    print(r.status_code,roundtrip,os.getppid(),os.getpid(),r.headers['Date'])
+    #print(r.status_code,roundtrip,os.getppid(),os.getpid(),r.headers['Date'])
+    logger.info("%s - %s - %s - %s - %s",r.status_code,roundtrip,os.getppid(),os.getpid(),r.headers['Date'])
 
 def f(domainname,delay):
     while(True):
@@ -21,7 +36,8 @@ def f(domainname,delay):
         time.sleep(delay)
 
 if __name__ == '__main__':
-    print("STATUS CODE,ROUND TRIP TIME,PARENT PROCESS ID,CHILD PROCESS ID,DATE")
+   # print("STATUS CODE,ROUND TRIP TIME,PARENT PROCESS ID,CHILD PROCESS ID,DATE")
+    logger.info("STATUS CODE - ROUND TRIP TIME - PARENT PROCESS ID - CHILD PROCESS ID - DATE")
     for i in range(thread):
         p = Process(target=f, args=(domain_name,sleep_time,))
         p.start()
