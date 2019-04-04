7d8bec5ee523cdabdb0cb49cd9d0440dc8c53ff5
Chan-Dru <chandrunitpy@gmail.com>
2019-04-04 09:29:19 +0000

    added namespace

--- server-conf-program/ServerConfAlterProgram.py
+++ server-conf-program/ServerConfAlterProgram.py
@@ -2,6 +2,7 @@ import os
 import time
 import configparser
 import logging
+import logging.handlers
 import codecs
 
 logger = logging.getLogger('client_program')
@@ -12,10 +13,16 @@ formatter = logging.Formatter('%(asctime)s, %(name)s, %(levelname)s, %(message)s
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+handler = logging.handlers.TimedRotatingFileHandler("server_log",'midnight',1)
+handler.suffix = "%Y-%m-%d"
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 config = configparser.ConfigParser()
 config.read('server-configuration.properties', encoding='utf-8')
 # config.readfp(codecs.open("server-configuration.properties", "r", "utf8"))
 
+namespace = str(config["Project"]["Name"])
 initialPodCount = int(config["Pod"]["InitialCount"])
 maxPodCount = int(config["Pod"]["MaxCount"])
 podStepCount = int(config["Pod"]["StepCount"])
@@ -24,10 +31,11 @@ commandFlag = str(config["Command"]["Flag"])
 command = str(config["Command"][commandFlag])
 print(type(initialPodCount))
 
-logger.info("program scale up in the range"+str(range(initialPodCount,maxPodCount,podStepCount)))
-logger.info("Initial Pod Count "+initialPodCount)
-for i in range(initialPodCount,maxPodCount+1,podStepCount):
+logger.info("program scale up in the range"+str(range(initialPodCount,maxPodCount+1,podStepCount)))
+logger.info("Initial Pod Count "+str(initialPodCount))
+command = command.replace("NAMESPACE",namespace)
+for i in range(initialPodCount,maxPodCount+1,podStepCount)[1:]:
     time.sleep(stepDuration)
     logger.info("Scale Pod to "+str(i))
-    os.system(command.replace("##",str(i)))
-    
+    os.system(command.replace("REPLICAS",str(i)))
+    #time.sleep(stepDuration)
