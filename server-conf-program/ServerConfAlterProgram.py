6423754729c4eb561159209bcc17a010b6726927
Chan-Dru <chandrunitpy@gmail.com>
2019-04-05 08:55:39 +0000

    added scale down support

--- server-conf-program/ServerConfAlterProgram.py
+++ server-conf-program/ServerConfAlterProgram.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import configparser
 import logging
@@ -13,29 +14,54 @@ formatter = logging.Formatter('%(asctime)s, %(name)s, %(levelname)s, %(message)s
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-handler = logging.handlers.TimedRotatingFileHandler("server_log",'midnight',1)
-handler.suffix = "%Y-%m-%d"
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
 
 config = configparser.ConfigParser()
 config.read('server-configuration.properties', encoding='utf-8')
 # config.readfp(codecs.open("server-configuration.properties", "r", "utf8"))
 
-namespace = str(config["Project"]["Name"])
-initialPodCount = int(config["Pod"]["InitialCount"])
-maxPodCount = int(config["Pod"]["MaxCount"])
-podStepCount = int(config["Pod"]["StepCount"])
-stepDuration = int(config["Pod"]["StepDurationSeconds"])
-commandFlag = str(config["Command"]["Flag"])
-command = str(config["Command"][commandFlag])
-print(type(initialPodCount))
 
-logger.info("program scale up in the range"+str(range(initialPodCount,maxPodCount+1,podStepCount)))
-logger.info("Initial Pod Count "+str(initialPodCount))
-command = command.replace("NAMESPACE",namespace)
-for i in range(initialPodCount,maxPodCount+1,podStepCount)[1:]:
-    time.sleep(stepDuration)
-    logger.info("Scale Pod to "+str(i))
-    os.system(command.replace("REPLICAS",str(i)))
-    #time.sleep(stepDuration)
+def server(time_string):
+    # fileName = str(sys.argv[1]+time_string)
+    fileName = "server_log"
+    handler = logging.handlers.TimedRotatingFileHandler(fileName,'midnight',1)
+    handler.suffix = "%Y-%m-%d"
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    namespace = str(config["Project"]["Name"])
+    initialPodCount = int(config["Pod"]["InitialCount"])
+    maxPodCount = int(config["Pod"]["FinalCount"])
+    podStepCount = int(config["Pod"]["StepCount"])
+    stepDuration = int(config["Pod"]["StepDurationSeconds"])
+    commandFlag = str(config["Command"]["Flag"])
+    command = str(config["Command"][commandFlag])
+    s3_bucket = str(config["Storage"]["s3_bucket"])
+    print(type(initialPodCount))
+    
+    if(initialPodCount<maxPodCount):
+        PodsList = range(initialPodCount,maxPodCount+1,podStepCount)[1:]     
+    else:
+        PodsList = range(maxPodCount,initialPodCount+1,podStepCount)[::-1][1:]
+    
+    logger.info("program scale up in the range"+str(PodsList))
+    logger.info("Initial Pod Count "+str(initialPodCount))
+    command = command.replace("NAMESPACE",namespace)
+        
+    for i in PodsList:
+        time.sleep(stepDuration)
+        logger.info("Scale Pod to "+str(i))
+        os.system(command.replace("REPLICAS",str(i)))
+        #time.sleep(stepDuration)
+    # logger.info("Scaling is done")
+    # os.system("aws s3 cp "+fileName+" "+s3_bucket)
+
+        
+if __name__=="__main__":
+    print(sys.argv)
+    if (len(sys.argv)==2):
+        named_tuple = time.localtime() # get struct_time
+        time_string = time.strftime("%m_%d_%Y_%H_%M", named_tuple)
+        server(time_string)
+    else:
+        print("Please provide the file name")
